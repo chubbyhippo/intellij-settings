18dadd89e3589b1312c1a448777d0d694b88351f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2023-09-21 15:55:47 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
