42837808488cca3a68d8138aae2c48723d7dbc29
ch <mkmsacc@outlook.com>
2024-03-09 23:40:02 +0700

    Add config path loop to install script
    The changes in this commit introduce a loop to output a list of configuration paths in the installation shell script. This will provide users with more detailed information about where configurations are stored during installation.

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
-idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
+#idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
+
+for config_path in "$HOME/Library/Application\ Support/JetBrains/IdeaIC*"  \
+         "$HOME/Library/Application\ Support/JetBrains/IntelliJIdea*"
+do
+echo "$config_path"
+done
