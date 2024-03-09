f95366a226d932d991ac1ec1a040f476c78ea68f
ch <mkmsacc@outlook.com>
2024-03-09 23:49:47 +0700

    Update IntelliJ-related paths and install templates
    The script now not only prints IntelliJ-related paths for IdeaIC and IntelliJIdea, but also checks if these directories exist. If they do, it copies a predefined set of templates to these locations. These updates provide easier and more uniform template management for IntelliJ-related projects.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 #idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
 
-for config_path in "$HOME/Library/Application\ Support/JetBrains/IdeaIC*"  \
-         "$HOME/Library/Application\ Support/JetBrains/IntelliJIdea*"
+for config_path in "$HOME"/Library/Application\ Support/JetBrains/IdeaIC*  \
+         "$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea*
 do
-echo "$config_path"
+  echo "$config_path"
+  if [ -d "$config_path" ]; then
+    # Install templates
+    mkdir -p "$config_path/templates"
+    cp -frv "config/templates"/* "$config_path/templates"
+  fi
 done
