df70105e7e558bd1769e0c1753f91ca7996f686f
ch <mkmsacc@outlook.com>
2024-03-10 12:18:00 +0700

    Correct indentation in install.sh script
    The 'for' loop in the install.sh script has been updated to correct inconsistent indentation. This correction improves code readability and consistency across the JetBrains applications' configuration paths.

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 #idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
 
 for config_path in "$HOME"/.config/JetBrains/IdeaIC* \
-         "$HOME"/.config/JetBrains/IdeaIC* \
-         "$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
-         "$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
-         "$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
-         "$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
+                  "$HOME"/.config/JetBrains/IdeaIC* \
+                  "$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
+                  "$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
+                  "$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
+                  "$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
 do
   echo "$config_path"
   if [ -d "$config_path" ]; then
