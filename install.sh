470546271e0f407d13fb83017e44c4f608414c78
ch <mkmsacc@outlook.com>
2024-03-10 12:15:24 +0700

    Update installation script to support Windows
    The installation script is updated to include paths for Windows. Previously, it only accommodated MacOS paths. Now it supports paths typically used in Windows for configuring JetBrains applications.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 #idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
 
-for config_path in "$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
+for config_path in "$HOME"/.config/JetBrains/IdeaIC* \
+         "$HOME"/.config/JetBrains/IdeaIC* \
+         "$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
          "$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
          "$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
          "$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
