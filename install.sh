fbf6f501a8485d7bbea042c157597e519e85ba46
ch <mkmsacc@outlook.com>
2024-03-10 12:49:57 +0700

    Remove plugin comment from install.sh
    The commented out line specifying the plugins to be installed has been removed from the install.sh script. Now, the script is cleaner and easier to read, as it only contains essential code.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-#idea installPlugins krasa.CpuUsageIndicator zielu.gittoolbox IdeaVIM org.jetbrains.IdeaVim-EasyMotion indent-rainbow.indent-rainbow izhangzhihao.rainbow.brackets com.andrey4623.rainbowcsv com.fwdekker.randomness org.sonarlint.idea
 
 for config_path in "$HOME"/.config/JetBrains/IdeaIC* \
                   "$HOME"/.config/JetBrains/IntelliJIdea* \
