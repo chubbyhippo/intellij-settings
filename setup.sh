f196d73bc51b782c5c7c89daa4f192f5235ca69f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-07-12 10:08:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,15 +15,15 @@ do
 
     # install keymaps
     mkdir -p "$config_path/keymaps"
-    curl https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
 
     # install settings
     mkdir -p "$config_path/options"
-    curl https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/editor.xml -o "$config_path/options/editor.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/editor.xml -o "$config_path/options/editor.xml"
 
     # install templates
     mkdir -p "$config_path/templates"
-    curl https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/templates/java.xml -o "$config_path/templates/java.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/templates/java.xml -o "$config_path/templates/java.xml"
 
   fi
 done
