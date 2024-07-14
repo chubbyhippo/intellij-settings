49cb184d54ac31ceb08a4b748f9a2168d7e39f7e
chubbyhippo <mkmsacc@outlook.com>
2024-07-14 11:09:15 +0700

    Add install-plugins-ic.sh script
    This update introduces the install-plugins-ic.sh script to automatically install JetBrains Toolbox plugins. The script detects the operating system and installs the necessary plugins using the respective command for IDEA. The plugins include "Builder Generator", "Lombok Plugin", "String Manipulation", "IdeaVIM", and more.

---
+++ install-plugins-ic.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# plugins
+cmd=idea
+if uname | grep -q "^MINGW"; then
+  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/idea.cmd
+fi
+
+$cmd installPlugins \
+"Builder Generator" \
+"Lombook Plugin" \
+"String Manipulation" \
+IdeaVIM \
+StringToolsPlugin \
+com.fwdekker.randomness \
+com.github.camork.fileExpander \
+com.mituuz.fuzzier \
+de.endrullis.idea.postfixtemplates \
+eu.theblob42.idea.whichkey \
+krasa.CpuUsageIndicator \
+me.bechberger.jfrplugin \
+mobi.hsz.idea.gitignore
+nl.bryanderidder.regexrenamefiles \
+org.jetbrains.jumpToLine \
+org.sonarlint.idea \
+zielu.gittoolbox \
