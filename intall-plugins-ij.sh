5aa6d275b6514f7d22bd685d6f26a653dc33b601
chubbyhippo <mkmsacc@outlook.com>
2024-07-14 11:09:23 +0700

    Add IntelliJ plugins installation script
    A new script has been added which automates the installation of various IntelliJ plugins. This makes setup more consistent and simplifies the process for new IntelliJ installations. The script has been configured to install a specific set of plugins relevant to our project requirements.

---
+++ intall-plugins-ij.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# plugins
+cmd=idea
+if uname | grep -q "^MINGW"; then
+  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/idea.cmd
+fi
+
+$cmd installPlugins \
+"Builder Generator" \
+"Lombook Plugin" \
+"String Manipulation" \
+IdeaVIM \
+StringToolsPlugin \
+com.andrey4623.rainbowcsv \
+com.fwdekker.randomness \
+com.github.camork.fileExpander \
+com.intellij.kubernetes \
+com.intellij.plugin.adernov.powershell \
+com.mituuz.fuzzier \
+de.endrullis.idea.postfixtemplates \
+eu.theblob42.idea.whichkey \
+indent-rainbow.indent-rainbow \
+izhangzhihao.rainbow.brackets.lite \
+krasa.CpuUsageIndicator \
+me.bechberger.jfrplugin \
+mobi.hsz.idea.gitignore
+nl.bryanderidder.regexrenamefiles \
+org.asciidoctor.intellij.asciidoc \
+org.jetbrains.jumpToLine \
+org.sonarlint.idea \
+zielu.gittoolbox \
