259e2271026d446776d21e124a1f463c4e653a01
chubbyhippo <mkmsacc@outlook.com>
2024-07-14 11:09:31 +0700

    Rename setup script and update file paths
    The setup.sh script has been renamed to setup-ic.sh. This change reflects its specific use case for the IntelliJ Community Edition (IC). To support this, the command that loops through file paths has been updated to exclude paths related to IntelliJ IDEA (Professional Edition), focusing solely on the Community Edition.

--- setup-ic.sh
+++ setup-ic.sh
@@ -3,11 +3,8 @@
 # configs
 for config_path in \
 "$HOME"/.config/JetBrains/IdeaIC* \
-"$HOME"/.config/JetBrains/IntelliJIdea* \
 "$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
-"$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
 "$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
-"$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
 
 do
   echo "$config_path"
