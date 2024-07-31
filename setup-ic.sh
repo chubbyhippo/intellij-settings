e33ee9cc62d41d5d3403abe0bb5d4036c07f9d30
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-07-31 21:11:58 +0700

    Update setup-ic.sh

--- setup-ic.sh
+++ setup-ic.sh
@@ -12,20 +12,20 @@ do
 
     # install keymaps
     mkdir -p "$config_path/keymaps"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
 
     # install settings
     mkdir -p "$config_path/options"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/editor.xml -o "$config_path/options/editor.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/options/editor.xml -o "$config_path/options/editor.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
     mkdir -p "$config_path/options/mac"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
     mkdir -p "$config_path/options/windows"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
 
     # install templates
     mkdir -p "$config_path/templates"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/config/templates/java.xml -o "$config_path/templates/java.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/config/templates/java.xml -o "$config_path/templates/java.xml"
 
   fi
 done
@@ -34,4 +34,4 @@ done
 curl -k https://raw.githubusercontent.com/chubbyhippo/ideavimrc/main/.ideavimrc -o ~/.ideavimrc
 
 # install plugins
-curl -k https://raw.githubusercontent.com/chubbyhippo/Intellij-settings/main/install-plugins-ic.sh | /usr/bin/env sh
+curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/main/install-plugins-ic.sh | /usr/bin/env sh
