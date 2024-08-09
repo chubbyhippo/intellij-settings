a97e1c1ea500b3a64f2070ae407697229aeba9af
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-08-09 22:00:30 +0700

    Update install-plugins-ij.sh

--- install-plugins-ij.sh
+++ install-plugins-ij.sh
@@ -7,26 +7,7 @@ if uname | grep -q "^MINGW"; then
 fi
 
 $cmd installPlugins \
-"Builder Generator" \
-"Lombook Plugin" \
-"String Manipulation" \
 IdeaVIM \
-StringToolsPlugin \
-com.andrey4623.rainbowcsv \
-com.fwdekker.randomness \
-com.github.camork.fileExpander \
-com.jetbrains.edu \
-com.intellij.kubernetes \
 com.intellij.ml.llm \
-com.intellij.plugin.adernov.powershell \
-de.endrullis.idea.postfixtemplates \
-dev.turingcomplete.intellijdevelopertoolsplugins \
 eu.theblob42.idea.whichkey \
-indent-rainbow.indent-rainbow \
-izhangzhihao.rainbow.brackets.lite \
-me.bechberger.jfrplugin \
-mobi.hsz.idea.gitignore \
-nl.bryanderidder.regexrenamefiles \
-org.asciidoctor.intellij.asciidoc \
-org.jetbrains.jumpToLine \
 org.sonarlint.idea
