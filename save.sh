9e5b0f7bcb98364167e5a0c6d50e84e87e4ee3d9
chubbyhippo <mkmsacc@outlook.com>
2024-10-31 21:41:51 +0700

    Add save.sh script to backup JetBrains IDE configurations
    This script iterates through potential JetBrains configuration paths and saves specific configuration files, such as keymaps and templates, into a local directory. It ensures that custom settings are backed up for both macOS and Windows environments.

---
+++ save.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# configs
+for config_path in \
+"$HOME"/.config/JetBrains/IdeaIC* \
+"$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
+"$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
+"$HOME"/.config/JetBrains/IntelliJIdea* \
+"$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
+"$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
+
+do
+  echo "$config_path"
+  if [ -d "$config_path" ]; then
+
+    # save keymaps
+    cat "$config_path"/keymaps/chubbyhippo.xml > ./config/keymaps/chubbyhippo.xml
+
+    # save options
+    cat "$config_path"/options/editor.xml > ./config/options/editor.xml
+    cat "$config_path"/options/postfixTemplates.xml > ./config/options/postfixTemplates.xml
+    cat "$config_path"/options/projectView.xml > ./config/options/projectView.xml
+    cat "$config_path"/options/mac/keymap.xml > ./config/options/mac/keymap.xml
+    cat "$config_path"/options/windows/keymap.xml > ./config/option/windows/keymap.xml
+
+    # save templates
+    cat "$config_path"/templates/java.xml > ./config/templates/java.xml
+
+  fi
+done
