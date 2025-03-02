7d8b0e7979b0fdf34f719de3cb4ccb4d70601c66
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-03-02 20:30:01 +0700

    Update setup-ij.sh

--- setup-ij.sh
+++ setup-ij.sh
@@ -19,6 +19,7 @@ do
     curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/editor.xml -o "$config_path/options/editor.xml"
     curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/postfixTemplates.xml -o "$config_path/options/postfixTemplates.xml"
     curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
+    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/ui.lnf.xml -o "$config_path/options/ui.lnf.xml"
     mkdir -p "$config_path/options/mac"
     curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
     mkdir -p "$config_path/options/windows"
