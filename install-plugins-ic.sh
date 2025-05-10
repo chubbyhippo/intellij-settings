109d43604c8429956bceec671132725ea5ab993b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-05-10 13:07:01 +0700

    Update install-plugins-ic.sh

--- install-plugins-ic.sh
+++ install-plugins-ic.sh
@@ -13,6 +13,7 @@ IdeaVIM \
 StringToolsPlugin \
 com.fwdekker.randomness \
 com.github.camork.fileExpander \
+com.julienphalip.ideavim.functiontextobj \
 com.julienphalip.ideavim.peekaboo \
 dev.flikas.idea.spring.boot.assistant.plugin
 dev.turingcomplete.intellijdevelopertoolsplugins \
