6a94da44fb24e02abef5807f4cea7e8b734404c4
chubbyhippo <mkmsacc@outlook.com>
2025-10-16 22:23:47 +0700

    Refactor `setup-ic.sh` to align indentation and improve readability of configuration loops

--- setup-ic.sh
+++ setup-ic.sh
@@ -2,42 +2,40 @@
 
 # config paths
 for config_path in \
-"$HOME"/.config/JetBrains/IdeaIC* \
-"$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
-"$HOME"/AppData/Roaming/JetBrains/IdeaIC*
+	"$HOME"/.config/JetBrains/IdeaIC* \
+	"$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
+	"$HOME"/AppData/Roaming/JetBrains/IdeaIC*; do
+	if [ -d "$config_path" ]; then
+		echo "$config_path"
+		# install code styles
+		mkdir -p "$config_path/codestyles"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/codestyles/Default.xml -o "$config_path/codestyles/Default.xml"
 
-do
-  if [ -d "$config_path" ]; then
-    echo "$config_path"
-    # install code styles
-    mkdir -p "$config_path/codestyles"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/codestyles/Default.xml -o "$config_path/codestyles/Default.xml"
+		# install keymaps
+		mkdir -p "$config_path/keymaps"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
 
-    # install keymaps
-    mkdir -p "$config_path/keymaps"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/keymaps/chubbyhippo.xml -o "$config_path/keymaps/chubbyhippo.xml"
+		# install options
+		mkdir -p "$config_path/options"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/editor.xml -o "$config_path/options/editor.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/laf.xml -o "$config_path/options/laf.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/colors.scheme.xml -o "$config_path/options/colors.scheme.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/postfixTemplates.xml -o "$config_path/options/postfixTemplates.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/ui.lnf.xml -o "$config_path/options/ui.lnf.xml"
+		mkdir -p "$config_path/options/mac"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
+		mkdir -p "$config_path/options/windows"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
 
-    # install options
-    mkdir -p "$config_path/options"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/editor.xml -o "$config_path/options/editor.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/laf.xml -o "$config_path/options/laf.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/colors.scheme.xml -o "$config_path/options/colors.scheme.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/postfixTemplates.xml -o "$config_path/options/postfixTemplates.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/projectView.xml -o "$config_path/options/projectView.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/ui.lnf.xml -o "$config_path/options/ui.lnf.xml"
-    mkdir -p "$config_path/options/mac"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/mac/keymap.xml -o "$config_path/options/mac/keymap.xml"
-    mkdir -p "$config_path/options/windows"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/options/windows/keymap.xml -o "$config_path/options/windows/keymap.xml"
+		# install templates
+		mkdir -p "$config_path/templates"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaPure.xml -o "$config_path/templates/javaPure.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaJUnit.xml -o "$config_path/templates/javaJUnit.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaMockito.xml -o "$config_path/templates/javaMockito.xml"
+		curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaSpring.xml -o "$config_path/templates/javaSpring.xml"
 
-    # install templates
-    mkdir -p "$config_path/templates"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaPure.xml -o "$config_path/templates/javaPure.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaJUnit.xml -o "$config_path/templates/javaJUnit.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaMockito.xml -o "$config_path/templates/javaMockito.xml"
-    curl -k https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/config/templates/javaSpring.xml -o "$config_path/templates/javaSpring.xml"
-
-  fi
+	fi
 done
 
 # install .ideavimrc
