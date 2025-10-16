4493c1851bcb90aeab89acf38a5dcf12ed37b4cd
chubbyhippo <mkmsacc@outlook.com>
2025-10-16 22:23:29 +0700

    Refactor `install.sh` script to simplify and improve readability of configuration loops

--- install.sh
+++ install.sh
@@ -2,30 +2,28 @@
 
 # configs
 for config_path in \
-"$HOME"/.config/JetBrains/IdeaIC* \
-"$HOME"/.config/JetBrains/IntelliJIdea* \
-"$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
-"$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
-"$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
-"$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*
+	"$HOME"/.config/JetBrains/IdeaIC* \
+	"$HOME"/.config/JetBrains/IntelliJIdea* \
+	"$HOME"/Library/Application\ Support/JetBrains/IdeaIC* \
+	"$HOME"/Library/Application\ Support/JetBrains/IntelliJIdea* \
+	"$HOME"/AppData/Roaming/JetBrains/IdeaIC* \
+	"$HOME"/AppData/Roaming/JetBrains/IntelliJIdea*; do
+	echo "$config_path"
+	if [ -d "$config_path" ]; then
 
-do
-  echo "$config_path"
-  if [ -d "$config_path" ]; then
+		# install keymaps
+		mkdir -p "$config_path/keymaps"
+		cp -frv "config/keymaps"/* "$config_path/keymaps"
 
-    # install keymaps
-    mkdir -p "$config_path/keymaps"
-    cp -frv "config/keymaps"/* "$config_path/keymaps"
+		# install settings
+		mkdir -p "$config_path/options"
+		cp -frv "config/options"/* "$config_path/options"
 
-    # install settings
-    mkdir -p "$config_path/options"
-    cp -frv "config/options"/* "$config_path/options"
+		# install templates
+		mkdir -p "$config_path/templates"
+		cp -frv "config/templates"/* "$config_path/templates"
 
-    # install templates
-    mkdir -p "$config_path/templates"
-    cp -frv "config/templates"/* "$config_path/templates"
-
-  fi
+	fi
 done
 
 # plugins
