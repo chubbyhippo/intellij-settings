2bcbf4321eed512ebe95bd271de2ad7adb3993b5
chubbyhippo <mkmsacc@outlook.com>
2025-12-26 23:20:35 +0700

    Refactor `install-plugins-ij.sh` for consistent indentation and formatting.

--- install-plugins-ij.sh
+++ install-plugins-ij.sh
@@ -2,22 +2,22 @@
 
 cmd=idea
 if uname | grep -q "^MINGW"; then
-	if command -v idea.cmd >/dev/null 2>&1; then
-		cmd=idea.cmd
-	elif command -v idea64 >/dev/null 2>&1; then
-		cmd=idea64
-	else
-		echo "Warning: neither idea.cmd nor idea64 found, falling back to idea"
-	fi
+  if command -v idea.cmd >/dev/null 2>&1; then
+    cmd=idea.cmd
+  elif command -v idea64 >/dev/null 2>&1; then
+    cmd=idea64
+  else
+    echo "Warning: neither idea.cmd nor idea64 found, falling back to idea"
+  fi
 fi
 
 $cmd installPlugins \
-	IdeaVIM \
-	com.github.camork.fileExpander \
-	com.intellij.ml.llm \
-	com.joshestein.ideavim-quickscope \
-	com.julienphalip.ideavim.peekaboo \
-	dev.turingcomplete.intellijdevelopertoolsplugins \
-	eu.theblob42.idea.whichkey \
-	org.asciidoctor.intellij.asciidoc \
-	org.jetbrains.jumpToLine
+  IdeaVIM \
+  com.github.camork.fileExpander \
+  com.intellij.ml.llm \
+  com.joshestein.ideavim-quickscope \
+  com.julienphalip.ideavim.peekaboo \
+  dev.turingcomplete.intellijdevelopertoolsplugins \
+  eu.theblob42.idea.whichkey \
+  org.asciidoctor.intellij.asciidoc \
+  org.jetbrains.jumpToLine
